e8f420fdba1422a8a210dd8d40b15bc8598c9cb0
Geist, Eugen <account@eugengeist.com>
2025-04-24 00:46:08 +0200

    code: start order entry data structures

--- src/ddtp/order_entry/data.py
+++ src/ddtp/order_entry/data.py
@@ -1,6 +1,13 @@
 """Data type definitions for order entry."""
 
+from decimal import Decimal
 from enum import StrEnum
+
+from pydantic import BaseModel
+
+from ddtp.marketdata.data import OrderBookSide
+
+import datetime as dt
 
 
 class OrderType(StrEnum):
@@ -11,3 +18,30 @@ class OrderType(StrEnum):
     STP = "stp"
     TAKE_PROFIT = "take_profit"
     TRAILING_STOP = "trailing_stop"
+
+
+class OrderBase(BaseModel):
+    sender_identifier: str
+    product_id: str
+
+
+class NewOrder(BaseModel):
+    side: OrderBookSide
+    order_type: OrderType
+    size: Decimal
+    limit_price: Decimal
+    stop_price: Decimal | None = (None,)
+    client_order_id: str | None = None
+
+
+class CancelOrder(BaseModel):
+    order_id: str | None
+    client_order_id: str | None = None
+
+
+class ModifyOrder(BaseModel):
+    order_id: str | None
+    client_order_id: str | None = None
+    process_before: dt.datetime | None = None
+    size: Decimal | None = None
+    limit_price: Decimal | None = None
--- src/ddtp/serialization/config.py
+++ src/ddtp/serialization/config.py
@@ -7,3 +7,5 @@ class KafkaClusterEnvVars(StrEnum):
 
 class KafkaTopics(StrEnum):
     MARKETDATA = "marketdata"
+    ORDER_ENTRY = "order_entry_send"
+    ORDER_ENTRY_FEEDBACK = "order_entry_feedback"
