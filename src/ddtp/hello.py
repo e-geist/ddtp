578cf8f025765c4cb35f78933f8957ebf0b62334
Geist, Eugen <account@eugengeist.com>
2025-04-24 00:46:08 +0200

    code: add first version of kraken derivatives feed

--- src/ddtp/hello.py
+++ src/ddtp/hello.py
@@ -1,22 +1,61 @@
 # Import WebSocket client library
-from websocket import create_connection
+import logging
+
+from ddtp.marketdata.kraken_derivatives.data import (
+    BookSubscribed,
+    BookSnapshot,
+    BookUpdate, Orderbook,
+)
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+from ddtp.marketdata.kraken_derivatives.websocket import KrakenDerivWebSocket
+
+logger = logging.getLogger("main")
+
+books = dict[str, Orderbook]()
+
+def _on_message_handler(message):
+    event = None
+    if "event" in message:
+        if "product_ids" in message:
+            event = BookSubscribed(**message)
+    elif "tickSize" in message:
+        event = BookSnapshot(**message)
+    elif "side" in message:
+        event = BookUpdate(**message)
+
+    if event:
+        # logger.info(f"Received message: {event}")
+        _build_book(event)
+
+def _build_book(book_data: BookUpdate | BookSnapshot):
+    book = books.get(book_data.product_id)
+    if not book:
+        book = Orderbook()
+        books[book_data.product_id] = book
+
+    match book_data:
+        case BookUpdate():
+            book.apply_update(book_data)
+        case BookSnapshot():
+            book.apply_snapshot(book_data)
+    logger.info(f"{book_data.product_id}: {book}")
+
+
 
 
 def main():
     # Connect to WebSocket API and subscribe to trade feed for XBT/USD and XRP/USD
-    ws = create_connection("wss://demo-futures.kraken.com/v2")
-    ws.send(
-        '''{"method":"subscribe", "params" : 
-{
-"channel": "level3", 
-"symbol": ["XBT/USD","XRP/USD"], 
-"token": "",
-},
-}'''
+    ws = KrakenDerivWebSocket(
+        "wss://demo-futures.kraken.com/ws/v1", _on_message_handler
     )
+    ws.subscribe_public("book", ["PI_XBTUSD", "PI_ETHUSD"])
     # Infinite loop waiting for WebSocket data
     while True:
-        print(ws.recv())
+        pass
 
 
 if __name__ == "__main__":
